94fcbe2882f39506d55051d2b6ded066b436f46a
Tom <trlc1996@hotmail.com>
2018-10-02 03:22:21 +0100

    Updated foldername

--- testVisualisation.py
+++ testVisualisation.py
@@ -15,15 +15,16 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 # Load dataset
+folderName = 'BrokenUpData'
 url = "ratings_Books.csv"
 names = ['user', 'item', 'rating', 'timestamp']
 
-directory = os.fsencode('BrokenUpData')
+directory = os.fsencode(folderName)
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".csv"): 
-        dataset = pandas.read_csv("BrokenUpData/" + filename, names=names)
+        dataset = pandas.read_csv(folderName+ "/" + filename, names=names)
         print(filename)
         scatter_matrix(dataset)
         plt.show()
