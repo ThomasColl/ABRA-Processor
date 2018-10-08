f8cfba20d19af0bdaf2808146de26b18d22d1da6
Tom <trlc1996@hotmail.com>
2018-10-08 09:35:51 +0100

    First Time Using PyCharm

--- pythonVersionCheck.py
+++ pythonVersionCheck.py
@@ -8,19 +8,25 @@ Created on Sun Sep 30 00:01:50 2018
 
 # Python version
 import sys
+
 print('Python: {}'.format(sys.version))
 # scipy
 import scipy
+
 print('scipy: {}'.format(scipy.__version__))
 # numpy
 import numpy
+
 print('numpy: {}'.format(numpy.__version__))
 # matplotlib
 import matplotlib
+
 print('matplotlib: {}'.format(matplotlib.__version__))
 # pandas
 import pandas
+
 print('pandas: {}'.format(pandas.__version__))
 # scikit-learn
 import sklearn
+
 print('sklearn: {}'.format(sklearn.__version__))
--- testVisualisation.py
+++ testVisualisation.py
@@ -1,19 +1,8 @@
 # Load libraries
-import pandas
 import os
-from pandas.plotting import scatter_matrix
-import pandas.plotting
+
 import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+import pandas.plotting
 
 # Load dataset
 folderName = 'BrokenUpData'
@@ -24,26 +13,25 @@ directory = os.fsencode(folderName)
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    if filename.endswith(".csv"): 
-        dataset = pandas.read_csv(folderName+ "/" + filename, names=names)
+    if filename.endswith(".csv"):
+        dataset = pandas.read_csv(folderName + "/" + filename, names=names)
         print(filename)
         dataset.plot(kind='scatter', x='timestamp', y='rating')
-        dataset.plot(kind='box', x='timestamp', y='rating')
-        #scatter_matrix(dataset)
-        #plt.scatter(dataset, x='timestamp', y='rating')
-        #plt.plot.scatter(dataset, x='timestamp', y='rating')
+        # dataset.plot(kind='k--', x='timestamp', y='rating')
+        # scatter_matrix(dataset)
+        # plt.scatter(dataset, x='timestamp', y='rating')
+        # plt.plot.scatter(dataset, x='timestamp', y='rating')
         plt.show()
-        #p=input("want to end?")
-        """if(p == "y"):
+        p = input("want to end?")
+        if (p == "y"):
             break;
-        #print(dataset.groupby('item').size())
-        #print(dataset.describe()) """
+        # print(dataset.groupby('item').size())
+        # print(dataset.describe())
     else:
         print("WHy is this here?")
 
+# dataset = pandas.read_csv(url, names=names)
 
-#dataset = pandas.read_csv(url, names=names)
-
-#print(dataset.head(20))
-#print(dataset.describe())
-#print(dataset.groupby('item').size())
+# print(dataset.head(20))
+# print(dataset.describe())
+# print(dataset.groupby('item').size())
