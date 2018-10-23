5a8f580f2172c498bc786ba4fc57813a1b9bd8cf
Tom <trlc1996@hotmail.com>
2018-10-23 20:01:07 +0100

    Refactored code to bring up to standard. Introduced classes and got working properly

--- testVisualisation.py
+++ testVisualisation.py
@@ -1,96 +1,80 @@
 # Load libraries
+import datetime
 import os
 
 import matplotlib.pyplot as plt
 import pandas.plotting
 
 
-def getYear(timestamp):
-    if years["1996"][0] <= timestamp < years["1997"][0]:
-        return "1996"
-    elif years["1997"][0] <= timestamp < years["1998"][0]:
-        return "1997"
-    elif years["1998"][0] <= timestamp < years["1999"][0]:
-        return "1998"
-    elif years["1999"][0] <= timestamp < years["2000"][0]:
-        return "1999"
-    elif years["2000"][0] <= timestamp < years["2001"][0]:
-        return "2000"
-    elif years["2001"][0] <= timestamp < years["2002"][0]:
-        return "2001"
-    elif years["2002"][0] <= timestamp < years["2003"][0]:
-        return "2002"
-    elif years["2003"][0] <= timestamp < years["2004"][0]:
-        return "2003"
-    elif years["2004"][0] <= timestamp < years["2005"][0]:
-        return "2004"
-    elif years["2005"][0] <= timestamp < years["2006"][0]:
-        return "2005"
-    elif years["2006"][0] <= timestamp < years["2007"][0]:
-        return "2006"
-    elif years["2007"][0] <= timestamp < years["2008"][0]:
-        return "2007"
-    elif years["2008"][0] <= timestamp < years["2009"][0]:
-        return "2008"
-    elif years["2009"][0] <= timestamp < years["2010"][0]:
-        return "2009"
-    elif years["2010"][0] <= timestamp < years["2011"][0]:
-        return "2010"
-    elif years["2011"][0] <= timestamp < years["2012"][0]:
-        return "2011"
-    elif years["2012"][0] <= timestamp < years["2013"][0]:
-        return "2012"
-    elif years["2013"][0] <= timestamp < years["2014"][0]:
-        return "2013"
-    elif years["2014"][0] <= timestamp:
-        return "2014"
-    else:
-        return "Huston " + str(timestamp)
+class yearlyDatatype:
+    def __init__(self, year):
+        self.year = int(year)
+        self.totalScore = 0
+        self.count = 0
+        self.average = 0
 
-    # Load dataset
+    def addReview(self, review):
+        self.totalScore += review
+        self.count += 1
 
-
-def makePlottable(yearDicts, type):
-    returnableYears = {
-        1996: 0,
-        1997: 0,
-        1998: 0,
-        1999: 0,
-        2000: 0,
-        2001: 0,
-        2002: 0,
-        2003: 0,
-        2004: 0,
-        2005: 0,
-        2006: 0,
-        2007: 0,
-        2008: 0,
-        2009: 0,
-        2010: 0,
-        2011: 0,
-        2012: 0,
-        2013: 0,
-        2014: 0,
-    }
-    counter = 1996
-    while (counter <= 2014):
-        returnableYears[counter] = yearDicts[str(counter)][type]
-        counter += 1
-    return returnableYears
-
-
-def averageOutYears(years):
-    counter = 1996
-    while (counter <= 2014):
-        l = list(years[str(counter)])
+    def setAverage(self):
         try:
-            l[3] = l[1] / l[2]
+            self.average = self.totalScore / self.count
         except:
-            l[3] = 0
-        years[str(counter)] = tuple(l)
-        counter += 1
+            print("getAverage Failure total score " + self.totalScore + " and count " + self.count)
 
-    return years
+    def returnDetails(self):
+        return tuple(self.year, self.totalScore, self.count, self.average)
+
+
+class normalisedDatatype:
+    def __init__(self):
+        self.dictOfYears = {}
+
+    def addYear(self, yearlyData):
+        self.dictOfYears[yearlyData.year] = yearlyData
+
+    def isYearThere(self, year):
+        if year in self.dictOfYears:
+            return True
+        else:
+            return False
+
+    def orderYears(self):
+        return sorted(self.dictOfYears)
+
+    def getAnnualAverages(self):
+        for key, value in self.dictOfYears.items():
+            value.setAverage()
+
+    def getPlottableDict(self):
+
+        plottableDict = {}
+        for key, value in self.dictOfYears.items():
+            plottableDict[key] = value.count
+        return plottableDict
+
+    def plotCount(self):
+        plottableDict = {}
+        for key, value in self.dictOfYears.items():
+            plottableDict[key] = value.count
+        plt.scatter(range(len(plottableDict)), sorted(plottableDict.values()))
+        plt.show()
+
+    def plotTotal(self):
+        plottableDict = {}
+        for key, value in self.dictOfYears.items():
+            plottableDict[key] = value.totalScore
+        plt.scatter(range(len(plottableDict)), sorted(plottableDict.values()))
+        plt.show()
+
+    def plotAverage(self):
+        self.getAnnualAverages()
+        plottableDict = {}
+        for key, value in self.dictOfYears.items():
+            plottableDict[key] = value.average
+        plt.scatter(range(len(plottableDict)), sorted(plottableDict.values()))
+        plt.show()
 
 
 folderName = 'BrokenUpData'
@@ -99,36 +83,13 @@ names = ['user', 'item', 'rating', 'timestamp']
 
 directory = os.fsencode(folderName)
 
-# Next plan: go through each line of the data,  check if its year, then add to the "years" total
-
-years = {
-    # "Year": (timestamp, total, count, average)
-    "1996": (820454400, 0, 0, 0),
-    "1997": (852076800, 0, 0, 0),
-    "1998": (883612800, 0, 0, 0),
-    "1999": (915148800, 0, 0, 0),
-    "2000": (946684800, 0, 0, 0),
-    "2001": (978307200, 0, 0, 0),
-    "2002": (1009843200, 0, 0, 0),
-    "2003": (1041379200, 0, 0, 0),
-    "2004": (1072915200, 0, 0, 0),
-    "2005": (1104537600, 0, 0, 0),
-    "2006": (1136073600, 0, 0, 0),
-    "2007": (1167609600, 0, 0, 0),
-    "2008": (1199145600, 0, 0, 0),
-    "2009": (1230768000, 0, 0, 0),
-    "2010": (1262304000, 0, 0, 0),
-    "2011": (1293840000, 0, 0, 0),
-    "2012": (1325376000, 0, 0, 0),
-    "2013": (1356998400, 0, 0, 0),
-    "2014": (1388534400, 0, 0, 0),
-}
 
 numberOfBrokenUpDataFiles = 90
 count = 1;
 meanAverage = 0
 shortestTimestamp = 0
 longestTimestamp = 0
+normalData = normalisedDatatype()
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".csv"):
@@ -136,30 +97,19 @@ for file in os.listdir(directory):
         print(filename)
         print(dataset["rating"].mean())
         for index, row in dataset.iterrows():
-            year = getYear(row['timestamp'])
-            if "Huston" in year:
-                print(getYear(row['timestamp']))
-                print("HUSTON WE HAVE A PROBLEM")
-                print(year)
-                break;
+            year = datetime.datetime.fromtimestamp(int(row['timestamp'])).strftime('%Y')
+
+            if normalData.isYearThere(int(year)) is True:
+                print("Date exists")
+                normalData.dictOfYears[int(year)].addReview(int(row["rating"]))
             else:
-                listOfYearData = list(years[year])
-                listOfYearData[1] = listOfYearData[1] + row["rating"]
-                listOfYearData[2] = listOfYearData[2] + 1
-                tupledListOfYearData = tuple(listOfYearData)
-                years[year] = tupledListOfYearData
-                print(years[year])
-                # break;
-    newYears = averageOutYears(years)
-    print("\n\n\n\n\n\n\n\n\n\n\n")
-    print(newYears)
-    plottableYears = makePlottable(newYears, 3)
-    plt.scatter(range(len(plottableYears)), list(plottableYears.values()))
-    plt.show()
-    plottableYears = makePlottable(newYears, 2)
-    plt.scatter(range(len(plottableYears)), list(plottableYears.values()))
-    plt.show()
+                print("Date does not exist")
+                normalData.addYear(yearlyDatatype(year))
+        normalData.plotCount()
+        normalData.plotAverage()
+        normalData.plotTotal()
     break;
+
     if count == 1:
         shortestTimestamp = int(dataset['timestamp'].min())
         longestTimestamp = int(dataset['timestamp'].max())
